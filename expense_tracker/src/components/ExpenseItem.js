a25b7e7ed7e2016b6ab442c77b92c7af182d3e80
Csilla Pósa <posa.csilla@gmail.com>
2021-09-17 15:36:14 +0200

    Added styling to ExpenseItem component

--- expense_tracker/src/components/ExpenseItem.js
+++ expense_tracker/src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <h2>Expense item</h2>
+        <div className="expense-item">
+            <div>17.09.2021.</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$300</div>
+            </div>
+        </div>
     );
 }
 
